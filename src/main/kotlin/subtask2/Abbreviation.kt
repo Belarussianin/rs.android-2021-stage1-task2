c37961ae72d6160d603d1507e4cb29092c61e20b
Kasheuski Arseni <30667022+Belarussianin@users.noreply.github.com>
2021-06-10 15:34:55 +0300

    Complete task

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,18 @@
 package subtask2
 
 class Abbreviation {
-
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var aArray = a.toUpperCase()
+        for (elemB in b) {
+            if (!aArray.contains(elemB, true)) {
+                return "NO"
+            }
+        }
+        for (elemA in aArray) {
+            if (!b.contains(elemA, true)) {
+                aArray = aArray.replace(elemA.toString(), "", true)
+            }
+        }
+        return if (aArray == b) "YES" else "NO"
     }
 }
--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,60 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
-
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+
+        fun sumIntOfArray(array: Array<*>): Int {
+            var sum = 0
+            for (i in array) {
+                if (i is Int) {
+                    sum += i
+                }
+            }
+            return sum
+        }
+
+        fun concatStringOfArray(array: Array<*>): String {
+            var line = ""
+            for (elem in array) {
+                if (elem is String) {
+                    line = line.plus(elem)
+                }
+            }
+            return line
+        }
+
+        fun parseClosestDateOfArray(array: Array<*>): String {
+            val now = LocalDate.now()
+            var maxDate = LocalDate.MIN
+            for (elem in array) {
+                if (elem is LocalDate) {
+                    elem.let {
+                        if (it.isBefore(now) && it.isAfter(maxDate)) {
+                            maxDate = it
+                        }
+                    }
+                }
+            }
+            return if (maxDate.isEqual(LocalDate.MIN)) throw Exception("No valid data passed") else maxDate.format(
+                DateTimeFormatter.ofPattern("d.M.y")
+            )
+        }
+
+        when (blockB) {
+            String::class -> {
+                return concatStringOfArray(blockA)
+            }
+            Int::class -> {
+                return sumIntOfArray(blockA)
+            }
+            LocalDate::class -> {
+                return parseClosestDateOfArray(blockA)
+            }
+        }
+        throw Exception("No valid data passed")
     }
 }

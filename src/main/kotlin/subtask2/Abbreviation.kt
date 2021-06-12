b4ffeaa02cfc3d96af5fb22551cb3d9f5c00398b
Kasheuski Arseni <30667022+Belarussianin@users.noreply.github.com>
2021-06-12 19:05:58 +0300

    Abbreviation update

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,17 +2,12 @@ package subtask2
 
 class Abbreviation {
     fun abbreviationFromA(a: String, b: String): String {
-        var aArray = a.toUpperCase()
-        for (elemB in b) {
-            if (!aArray.contains(elemB, true)) {
-                return "NO"
-            }
-        }
+        var aArray = a
         for (elemA in aArray) {
             if (!b.contains(elemA, true)) {
                 aArray = aArray.replace(elemA.toString(), "", true)
             }
         }
-        return if (aArray == b) "YES" else "NO"
+        return if (aArray.equals(b,true)) "YES" else "NO"
     }
 }
